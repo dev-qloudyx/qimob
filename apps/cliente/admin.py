6edf2dc6c0876f7c67587d3ccc13b573f0c2aef6
Victor Costa <victor.costa@qloudyx.pt>
2023-11-08 14:58:31 +0000

    ADD cliente migrations

--- apps/cliente/admin.py
+++ apps/cliente/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from apps.cliente.models import Client, ClientDoc
+from apps.cliente.models import Cliente, ClienteDoc
 
 # Register your models here.
 
-admin.site.register(Client)
-admin.site.register(ClientDoc)
+admin.site.register(Cliente)
+admin.site.register(ClienteDoc)
 
