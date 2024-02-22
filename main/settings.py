c1bc72f4f758c8ffa1a0583bad7cabf80b515f29
muffs98 <146010932+muffs98@users.noreply.github.com>
2024-02-22 19:02:07 +0000

    Update templates
    -Update to the base template;
    -Added home template

--- main/settings.py
+++ main/settings.py
@@ -100,24 +100,24 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "qimobi",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('POSTGRES_ENGINE',
-#                     'django.db.backends.postgresql'),
-#         'NAME': os.getenv('POSTGRES_DATABASE'),
-#         'USER': os.getenv('POSTGRES_USER'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#         'HOST': os.getenv('POSTGRES_HOST'),
-#         'PORT': os.getenv('POSTGRES_PORT'),
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "qimobi",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('POSTGRES_ENGINE',
+                    'django.db.backends.postgresql'),
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
 
 
 # Password validation
