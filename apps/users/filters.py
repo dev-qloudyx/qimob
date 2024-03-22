6e9281e1a261a4c9a987b751d5e1c57807b6a0ac
Ana Lopes <ana.lopes@qloudyx.pt>
2024-03-22 12:03:44 +0000

    add: filter active/inactive

--- apps/users/filters.py
+++ apps/users/filters.py
@@ -1,21 +1,32 @@
+from django import forms
 import django_filters
 from django.db.models import Q
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 from .models import Profile
 
 
 class UserFilter(django_filters.FilterSet):
-
-    # search = django_filters.CharFilter(method='imovel_filter', label='Pesquisar')
+    user__email = django_filters.CharFilter(lookup_expr='icontains', field_name='user__email', label='E-mail')
+    user__username = django_filters.CharFilter(lookup_expr='icontains', field_name='user__username', label='Username')
 
     class Meta:
         model = Profile
         fields = {
-            'user__email': ['icontains'],
-            'user__username': ['icontains'],
-            'user__role': ['exact']
+            'user__role': ['exact'],
+            'user__is_active': ['exact'],
         }
+
+
         
+# class IBVlogFilter(django_filters.FilterSet):
+    # search = django_filters.CharFilter(method='imovel_filter', label='Pesquisar')
+    
+    
+#     class Meta:
+#         model = IBBlockStatus
+#         fields = ['name', 'block__history', 'block__book']
+
 
     # def general_search_filter(self, queryset, name, value):
     #     return Imovel.objects.filter(square_footage__icontains=value)
