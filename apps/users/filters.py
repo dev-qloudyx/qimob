a1bb08f98566098bbd64f4ac92c1450d24c19a1a
Ana Lopes <ana.lopes@qloudyx.pt>
2024-03-22 12:37:34 +0000

    add: filter by leader

--- apps/users/filters.py
+++ apps/users/filters.py
@@ -3,19 +3,25 @@ import django_filters
 from django.db.models import Q
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from .models import Profile
+from .models import Profile, TeamLeader, Teams
 
 
 class UserFilter(django_filters.FilterSet):
     user__email = django_filters.CharFilter(lookup_expr='icontains', field_name='user__email', label='E-mail')
     user__username = django_filters.CharFilter(lookup_expr='icontains', field_name='user__username', label='Username')
+    user_leaders = django_filters.ChoiceFilter(method='leaders_search_filter', label='Chefes Equipa', choices=[[t.pk, t] for t in TeamLeader.objects.all()])
 
     class Meta:
         model = Profile
         fields = {
-            'user__role': ['exact'],
+            'user__role': ['exact'], 
             'user__is_active': ['exact'],
-        }
+            }
+
+    def leaders_search_filter(self, queryset, name, value):
+        leader_id = value
+        members = Teams.objects.filter(team_leader_id=leader_id)
+        return Profile.objects.filter(user_id__in=members.values_list('team_member', flat=True))
 
 
         
