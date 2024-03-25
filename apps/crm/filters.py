755c89f90a1eff2a38794aad8d4fe215d85e6993
Ana Lopes <ana.lopes@qloudyx.pt>
2024-03-25 12:11:39 +0000

    fix: lead filter status

--- apps/crm/filters.py
+++ apps/crm/filters.py
@@ -5,7 +5,7 @@ from .models import Lead, Client, LeadStatus, LeadType
 
 class LeadTypeFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='lead', lookup_expr='icontains')
-    status = django_filters.ChoiceFilter(field_name='status', choices=LeadStatus.objects.values_list('status', 'status'))
+    status = django_filters.ChoiceFilter(field_name='status', choices=LeadStatus.objects.values_list('status', 'status__name').distinct())
 
 
     class Meta:
