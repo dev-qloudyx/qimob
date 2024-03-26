a59d898ac8be7ba1d48237bc6846facaabcf0001
Ana Lopes <ana.lopes@qloudyx.pt>
2024-03-26 16:27:23 +0000

    fix: user list server error

--- apps/users/filters.py
+++ apps/users/filters.py
@@ -9,7 +9,7 @@ class UserFilter(django_filters.FilterSet):
     user__username = django_filters.CharFilter(lookup_expr='icontains', field_name='user__username', label=_('By Username'))
     user__is_active = django_filters.BooleanFilter(lookup_expr='exact', label=_('Is Active'))
     user__role = django_filters.ChoiceFilter(lookup_expr='exact', label=_('By Role'), choices=[[t.pk, t] for t in UserRole.objects.all()])
-    user_leaders = django_filters.ChoiceFilter(method='leaders_search_filter', label=_('By Team Leader'), choices=[[t.pk, t] for t in TeamLeader.objects.all()])
+    user_leaders = django_filters.ChoiceFilter(method='leaders_search_filter', label=_('By Team Leader'), choices=lambda: [[t.pk, str(t)] for t in TeamLeader.objects.all()] if TeamLeader.objects.exists() else [('', _('No Team Leaders available'))])
 
     class Meta:
         model = Profile
