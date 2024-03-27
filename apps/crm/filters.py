7b6d2a11378a08d2c72c65a46106876ba93c1848
muffs98 <146010932+muffs98@users.noreply.github.com>
2024-03-27 12:36:55 +0000

    Fix filters

--- apps/crm/filters.py
+++ apps/crm/filters.py
@@ -6,8 +6,11 @@ from .models import Lead, Client, LeadStatus, LeadType
 class LeadTypeFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='lead__short_name', lookup_expr='icontains')
     status = django_filters.ChoiceFilter(field_name='status', choices=LeadStatus.objects.values_list('status', 'status__name').distinct())
+    leadtype = django_filters.ChoiceFilter(field_name='lead__leadtype', choices=LeadStatus.objects.values_list('lead__leadtype', 'lead__leadtype__type').distinct())
+    district = django_filters.ChoiceFilter(field_name='lead__district', choices=LeadStatus.objects.values_list('lead__district', 'lead__district__DESIG').distinct())
+    county = django_filters.ChoiceFilter(field_name='lead__county', choices=LeadStatus.objects.values_list('lead__county', 'lead__county__DESIG').distinct())
 
 
     class Meta:
         model = LeadStatus
-        fields = ['status', 'name']
+        fields = ['status', 'name', 'leadtype', 'district', 'county']
